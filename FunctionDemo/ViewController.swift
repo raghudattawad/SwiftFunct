e0583cd9474c94842d44560e1f0a0e850dedf6cf
Raghavendra Dattawad <raghavendra.d@wickedride.com>
2018-06-11 04:14:07 -0700

    Functuons call swift demo

--- FunctionDemo/ViewController.swift
+++ FunctionDemo/ViewController.swift
@@ -13,7 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+        
+        /// we use dot operator for method calling ///
+    self.method()
+   
+    //// call method2 ///
+        
+        let myName = self.method2(name: "Raghu")
+    print(myName)
+    
+}
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let myDetails = self.differentTypesOfParameter(name: "Raghavendra", id: 167)
+        
+        print(myDetails)
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +40,25 @@ class ViewController: UIViewController {
     }
 
 
+    func method(){
+        
+       print("method1")
+        
+    }
+  
+    
+    ///parameter String
+    
+    func method2(name:String) -> String {
+        
+        
+        return name
+    }
+    
+    func differentTypesOfParameter(name:String, id:Int) ->  String{
+        
+        return "\(name)\(id)"
+    }
+    
 }
 
